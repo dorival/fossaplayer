60663bfb497ab650543b76780236091078e74c34
Dorival Neto <dorivaltn@gmail.com>
2020-09-18 18:23:43 -0400

    Adding better fonts

--- src/app/components/player/Player.js
+++ src/app/components/player/Player.js
@@ -6,6 +6,7 @@ class Player extends React.Component {
   render() {
     return (
       <>
+        <h1>Player!</h1>
         <button>PREV</button>
         <button>TOGGLE PLAY</button>
         <button>NEXT</button>
