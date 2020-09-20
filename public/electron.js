79fe37728911e4e6699774732949109332316f59
Dorival Neto <dorivaltn@gmail.com>
2020-09-20 01:21:18 -0400

    First POC for Authentication on MS Graph

--- public/electron.js
+++ public/electron.js
@@ -1,147 +1,229 @@
-const { app, BrowserWindow, shell, ipcMain, Menu, TouchBar } = require('electron');
-const { TouchBarButton, TouchBarLabel, TouchBarSpacer } = TouchBar;
-
-const path = require('path');
+const {
+  BrowserWindow,
+  Menu,
+	app,
+  shell,
+  ipcMain,
+} = require('electron');
 const isDev = require('electron-is-dev');
+const path = require('path');
+const { PublicClientApplication } = require('msal-electron-poc');
+const fetch = require('electron-fetch').default;
+
 
 let mainWindow;
 
+let accessToken;
+let msalApp;
+
 const createWindow = () => {
-	mainWindow = new BrowserWindow({
-		backgroundColor: '#F7F7F7',
-		minWidth: 880,
-		show: false,
-		titleBarStyle: 'hidden',
-		webPreferences: {
-			nodeIntegration: false, // is default value after Electron v5
-			contextIsolation: true, // protect against prototype pollution
-			preload: __dirname + '/preload.js',
-		},
-		height: 860,
-		width: 1280,
-	});
+  mainWindow = new BrowserWindow({
+    backgroundColor: '#F7F7F7',
+    minWidth: 880,
+    show: false,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      preload: __dirname + '/preload.js',
+    },
+    height: 860,
+    width: 1280,
+  });
 
-	mainWindow.loadURL(
-		isDev
-			? 'http://localhost:3000'
-			: `file://${path.join(__dirname, '../build/index.html')}`,
-	);
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 
-	if (isDev) {
+  if (isDev) {
+    const {
+      default: installExtension,
+      REACT_DEVELOPER_TOOLS,
+      REDUX_DEVTOOLS,
+    } = require('electron-devtools-installer');
 
-		// mainWindow.webContents.openDevTools();
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => {
+        console.log(`Added Extension: ${name}`);
+      })
+      .catch((err) => {
+        console.log('An error occurred: ', err);
+      });
 
-		const {
-			default: installExtension,
-			REACT_DEVELOPER_TOOLS,
-			REDUX_DEVTOOLS,
-		} = require('electron-devtools-installer');
+    installExtension(REDUX_DEVTOOLS)
+      .then((name) => {
+        console.log(`Added Extension: ${name}`);
+      })
+      .catch((err) => {
+        console.log('An error occurred: ', err);
+      });
+  }
 
-		installExtension(REACT_DEVELOPER_TOOLS)
-			.then(name => {
-				console.log(`Added Extension: ${name}`);
-			})
-			.catch(err => {
-				console.log('An error occurred: ', err);
-			});
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
 
-		installExtension(REDUX_DEVTOOLS)
-			.then(name => {
-				console.log(`Added Extension: ${name}`);
-			})
-			.catch(err => {
-				console.log('An error occurred: ', err);
-			});
-	}
-
-	mainWindow.once('ready-to-show', () => {
-		mainWindow.show();
-
-		ipcMain.on('open-external-window', (event, arg) => {
-			shell.openExternal(arg);
-		});
-	});
+    ipcMain.on('open-external-window', (event, arg) => {
+      shell.openExternal(arg);
+    });
+  });
 };
 
 const generateMenu = () => {
-	const template = [
-		{
-			label: 'File',
-			submenu: [{ role: 'about' }, { role: 'quit' }],
-		},
-		{
-			label: 'Edit',
-			submenu: [
-				{ role: 'undo' },
-				{ role: 'redo' },
-				{ type: 'separator' },
-				{ role: 'cut' },
-				{ role: 'copy' },
-				{ role: 'paste' },
-				{ role: 'pasteandmatchstyle' },
-				{ role: 'delete' },
-				{ role: 'selectall' },
-			],
-		},
-		{
-			label: 'View',
-			submenu: [
-				{ role: 'reload' },
-				{ role: 'forcereload' },
-				{ role: 'toggledevtools' },
-				{ type: 'separator' },
-				{ role: 'resetzoom' },
-				{ role: 'zoomin' },
-				{ role: 'zoomout' },
-				{ type: 'separator' },
-				{ role: 'togglefullscreen' },
-			],
-		},
-		{
-			role: 'window',
-			submenu: [{ role: 'minimize' }, { role: 'close' }],
-		},
-		{
-			role: 'help',
-			submenu: [
-				{
-					click() {
-						require('electron').shell.openExternal(
-							'https://getstream.io/winds',
-						);
-					},
-					label: 'Learn More',
-				},
-				{
-					click() {
-						require('electron').shell.openExternal(
-							'https://github.com/GetStream/Winds/issues',
-						);
-					},
-					label: 'File Issue on GitHub',
-				},
-			],
-		},
-	];
+  const template = [
+    {
+      label: 'File',
+      submenu: [{ role: 'about' }, { role: 'quit' }],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { role: 'selectall' },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
+    },
+    {
+      role: 'window',
+      submenu: [{ role: 'minimize' }, { role: 'close' }],
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          click() {
+            require('electron').shell.openExternal(
+              'https://getstream.io/winds'
+            );
+          },
+          label: 'Learn More',
+        },
+        {
+          click() {
+            require('electron').shell.openExternal(
+              'https://github.com/GetStream/Winds/issues'
+            );
+          },
+          label: 'File Issue on GitHub',
+        },
+      ],
+    },
+  ];
 
-	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 };
 
 app.on('ready', () => {
-	createWindow();
+  createWindow();
 	generateMenu();
+
+	configureAuthentication();
+	listenForAcquireToken();
+	listenForUserInfo();
 });
 
 app.on('window-all-closed', () => {
-	app.quit();
+  app.quit();
 });
 
 app.on('activate', () => {
-	if (mainWindow === null) {
-		createWindow();
-	}
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 ipcMain.on('load-page', (event, arg) => {
-	mainWindow.loadURL(arg);
+  mainWindow.loadURL(arg);
 });
+
+// --------------- //
+
+function configureAuthentication() {
+  msalApp = new PublicClientApplication({
+    clientId: 'MY_API_KEY_HERE',
+  });
+}
+
+// Sets a listener for the AcquireToken event that when triggered
+// performs the authorization code grant flow
+function listenForAcquireToken() {
+  ipcMain.on('AcquireToken', () => {
+    getAccessToken();
+  });
+}
+
+async function getAccessToken() {
+  const tokenRequest = {
+    scopes: ['user.read', 'files.read'],
+  };
+
+  try {
+		accessToken = await msalApp.acquireToken(tokenRequest);
+		console.log('Got Token', accessToken);
+		mainWindow.webContents.send('IsAuthenticated', true);
+  } catch (error) {
+		mainWindow.webContents.send('IsAuthenticated', false);
+    console.error(error);
+  }
+}
+
+function listenForUserInfo() {
+	ipcMain.on('AcquireUserInfo', () => {
+		if (accessToken) {
+			showUserInfo();
+		} else {
+			mainWindow.webContents.send('UserInfo', { username: 'Not Authenticated' });
+		}
+	});
+}
+
+function showUserInfo() {
+	fetch('https://graph.microsoft.com/v1.0/me', {
+		method: 'GET',
+		headers: {
+			'Authorization': `Bearer ${accessToken}`
+		}
+	})
+	.then((response) => {
+		if (response && response.ok === true) {
+			response
+				.json()
+				.then(body => {
+					const userInfo = {
+						username: `${body.displayName} (${body.userPrincipalName})`
+					}
+					mainWindow.webContents.send('UserInfo', userInfo);
+				})
+		} else {
+			mainWindow.webContents.send('UserInfo', {
+				username: 'Not Authenticated'
+			});
+		}
+		
+	})
+	.catch((error) => {
+		mainWindow.webContents.send('UserInfo',  { username: error.description });
+	});
+	
+}
--- src/app/components/player/Player.js
+++ src/app/components/player/Player.js
@@ -1,15 +1,41 @@
-
 import React from 'react';
 
 class Player extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      username: 'Anon'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    window.api.receive('IsAuthenticated', (isAuthenticated) => {
+      console.log('[IsAuthenticated] Received response', isAuthenticated);
+      if(isAuthenticated) {
+        window.api.send('AcquireUserInfo');
+      } else {
+        console.log('Cant authenticate...')
+      }
+    });
+
+    window.api.receive('UserInfo', (userInfo) => {
+      this.setState({
+        username: userInfo.username
+      });
+    });
+  }
+
+  handleClick() {
+    window.api.send('AcquireToken');
+  }
+
   render() {
     return (
       <>
-        <h1>Player!</h1>
-        <button>PREV</button>
-        <button>TOGGLE PLAY</button>
-        <button>NEXT</button>
+        <h1>Hello {this.state.username}!</h1>
+        <button onClick={this.handleClick}>Authenticate</button>
       </>
     );
   }
